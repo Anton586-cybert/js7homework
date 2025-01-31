39bc758f0438854274ab865cc1021b7f7ed657c9
antonkorol2008 <antonkorol6r@gmail.com>
2025-01-31 11:33:25 +0200

    k

--- style.js
+++
@@ -1,8 +0,0 @@
-let array = [12, 45, 7, 89, 23, 56, 90, 34, 67, 15];
-let max = array[0];
-for (let i = 1; i < array.length; i++) {
-  if (array[i] > max) {
-    max = array[i];
-  }
-}
-console.log(max);
