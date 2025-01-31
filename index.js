555407271f18683159165946bea4e66dc85fa04a
antonkorol2008 <antonkorol6r@gmail.com>
2025-01-31 11:27:12 +0200

    s

---
+++ index.js
@@ -0,0 +1,13 @@
+let arr = [23, 45, 21];
+arr[1] = 10;
+console.log(arr);
+const car = ["Opel", "porshe", "bmw"];
+car[3] = "volkswagen";
+console.log(car[3]);
+console.log(car);
+let array = ["яблуко", "груша", "кіт", "машина", "дерево"];
+for (let str of array) {
+  if (str.length > 5) {
+    console.log(str);
+  }
+}
--- style.js
+++ style.js
@@ -1,2 +1,8 @@
-
-
+let array = [12, 45, 7, 89, 23, 56, 90, 34, 67, 15];
+let max = array[0];
+for (let i = 1; i < array.length; i++) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
+console.log(max);
